d7097c398308c36babbc38c0f5b6ba4995cf8be0
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-03-08 16:31:45 +0800

    Pre-pull

--- public/js/crawler.js
+++ public/js/crawler.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
 
         loadCrawlers();
     });
+
     $('#state').bind('select', loadUsCities());
     $('#scrape').click(scrapeThis);
     $('#download').click(downloadCSV);
